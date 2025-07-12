9e8b4648d17eefdf0792289c36341e5b7f00d8c0
Cole Smith-Evans <sparklesonic@gmail.com>
2025-07-12 00:52:48 -0300

    updated enviros, tested some spring bones

--- test/area_generation_test.gd
+++ test/area_generation_test.gd
@@ -1,8 +1,10 @@
 extends Node3D
+@export var startingArea : String
 
 func _ready():
 	GameManager.run_area_generation()
 	loading_time()
 	
 func loading_time() -> void:
-	GameManager.load_area(GameManager.startingArea)
+	if !startingArea: startingArea = GameManager.startingArea
+	GameManager.load_area(startingArea)
